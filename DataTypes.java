028f60d47f52f441d58f39f36764a866a12c1f30
Vinisha-Luhar <vinishaluhar11@gmail.com>
2025-11-04 01:21:54 +0530

    Solved the Sort Colors problem of leetcode 75

---
+++ DataTypes.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        int i=5;
+        double d=8.0;
+        String s="Hello";
+        int i1;
+        double d2;
+        String s2;
+        Scanner scanner = new Scanner(System.in);
+        i1=scanner.nextInt();
+        d2=scanner.nextDouble();
+        s2=scanner.next();
+        System.out.println(i1);
+        System.out.println(d2);
+        System.out.println(s2);
+    }
+}
---
+++ LeftPyramidPattern.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class LeftPyramidPattern {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of rows OR columns");
+        int n = scanner.nextInt();
+        
+    }
+}
