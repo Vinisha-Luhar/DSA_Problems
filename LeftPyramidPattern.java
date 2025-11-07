406502b540314235e509303cbb92143d036723a0
Vinisha-Luhar <vinishaluhar11@gmail.com>
2025-11-07 23:31:47 +0530

    Add Left Pyramid Pattern

--- LeftPyramidPattern.java
+++ LeftPyramidPattern.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class LeftPyramidPattern {
+    //Left Pyramid Pattern
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the number of rows OR columns");
